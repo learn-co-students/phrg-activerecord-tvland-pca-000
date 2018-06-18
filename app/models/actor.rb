65ebe28097fda5eb130e67b2702be7eaf6a67269
Daniel-Mack <daniel.mack@powerhrg.com>
2018-06-18 11:31:11 -0400

    finished lab

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = "#{first_name + " " + last_name}"
+  end
+
+  def list_roles
+    name = self.characters.first.name
+    show = self.shows.first.name
+    role = "#{name} - #{show}"
+  end
 end
