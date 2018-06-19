4e44822d29ab2a9c689fef57d7d069ae305d88fa
bkrigel <benjamin.krigel@gmail.com>
2018-06-19 17:17:27 -0400

    Super gratifying lab. Loved it.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |character|
+      full_role = "#{character.name} - #{character.show.name}"
+      roles << full_role
+    end
+    roles
+  end
 end
