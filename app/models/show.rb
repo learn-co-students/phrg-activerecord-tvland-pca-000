2efe41f6cffad0de25efd4fdcf160a4a98a2a84f
gillianwenhold <gwenhold@mac.com>
2019-07-07 11:29:40 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(network)
+    self.network = Network.new(network)
+  end
 end
---
+++ db/migrate/006_add_shows_attributes.rb
@@ -0,0 +1,5 @@
+class AddShowsAttributes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :shows, :genre, :string
+  end
+end
